64b2022fd0c89fdac975aa079351cddfcbc527b5
fanling1970 <zghqidai2008@sina.com>
2024-09-06 18:55:21 +0800

    Update Settings_jdc-ax6600.sh

--- Scripts/Settings_jdc-ax6600.sh
+++ Scripts/Settings_jdc-ax6600.sh
@@ -5,23 +5,13 @@
 echo "CONFIG_PACKAGE_luci-app-store=y"  >> ./.config
 
 echo "CONFIG_PACKAGE_luci-app-ssr-plus=y"  >> ./.config
-# echo "CONFIG_PACKAGE_adbyby=y"  >> ./.config
 echo "CONFIG_PACKAGE_cpufreq=y"  >> ./.config
-#echo "CONFIG_PACKAGE_cpulimit=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-app-accesscontrol=y"  >> ./.config
-# echo "CONFIG_PACKAGE_luci-app-adbyby-plus=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-app-cpufreq=y"  >> ./.config
-#echo "CONFIG_PACKAGE_luci-app-cpulimit=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-app-disks-info=y"  >> ./.config
-#echo "CONFIG_PACKAGE_luci-app-onliner=y"  >> ./.config
-# echo "CONFIG_PACKAGE_luci-app-openvpn=y"  >> ./.config
-# echo "CONFIG_PACKAGE_luci-app-timecontrol=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-app-timewol=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-app-ttyd=y"  >> ./.config
-# echo "CONFIG_PACKAGE_luci-app-v2raya=y"  >> ./.config
-# echo "CONFIG_PACKAGE_luci-app-verysync=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-app-wrtbwmon=y"  >> ./.config
-#echo "CONFIG_PACKAGE_luci-app-zerotier=y"  >> ./.config
 
 echo "CONFIG_PACKAGE_luci-app-passwall=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Hysteria=y"  >> ./.config
@@ -30,7 +20,6 @@ echo "CONFIG_PACKAGE_iptables-mod-socket=y"  >> ./.config
 echo "CONFIG_PACKAGE_iptables-mod-iprange=y"  >> ./.config
 echo "CONFIG_PACKAGE_iptables-mod-conntrack-extra=y"  >> ./.config
 
-@echo "CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y"  >> ./.config
 echo "CONFIG_ARIA2_BITTORRENT=y"  >> ./.config
 echo "CONFIG_ARIA2_NOXML=y"  >> ./.config
 echo "CONFIG_ARIA2_OPENSSL=y"  >> ./.config
@@ -44,31 +33,19 @@ echo "CONFIG_PACKAGE_luci-app-eqos=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-i18n-eqos-zh-cn=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-app-openclash=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-i18n-openclash-zh-cn=y"  >> ./.config
-# echo "CONFIG_PACKAGE_luci-app-poweroff=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-i18n-accesscontrol-zh-cn=y"  >> ./.config
-# echo "CONFIG_PACKAGE_luci-i18n-adbyby-plus-zh-cn=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-i18n-cpufreq-zh-cn=y"  >> ./.config
-#echo "CONFIG_PACKAGE_luci-i18n-cpulimit-zh-cn=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-i18n-disks-info-zh-cn=y"  >> ./.config
-# echo "CONFIG_PACKAGE_luci-i18n-openvpn-zh-cn=y"  >> ./.config
-# echo "CONFIG_PACKAGE_luci-i18n-timecontrol-zh-cn=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-i18n-timewol-zh-cn=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-i18n-ttyd-zh-cn=y"  >> ./.config
-# echo "CONFIG_PACKAGE_luci-i18n-v2raya-zh-cn=y"  >> ./.config
-# echo "CONFIG_PACKAGE_luci-i18n-verysync-zh-cn=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-i18n-wrtbwmon-zh-cn=y"  >> ./.config
-#echo "CONFIG_PACKAGE_luci-i18n-zerotier-zh-cn=y"  >> ./.config
 echo "CONFIG_PACKAGE_ttyd=y"  >> ./.config
-# echo "CONFIG_PACKAGE_v2raya=y"  >> ./.config
-# echo "CONFIG_PACKAGE_verysync=y"  >> ./.config
 echo "CONFIG_PACKAGE_wrtbwmon=y"  >> ./.config
-#echo "CONFIG_PACKAGE_zerotier=y"  >> ./.config
 
 echo "CONFIG_PACKAGE_docker=y"  >> ./.config
 echo "CONFIG_PACKAGE_dockerd=y"  >> ./.config
 echo "CONFIG_PACKAGE_docker-compose=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-lib-docker=y"  >> ./.config
-#echo "CONFIG_PACKAGE_luci-app-docker=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-app-dockerman=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-i18n-dockerman-zh-cn=y"  >> ./.config
 
@@ -80,10 +57,8 @@ echo "CONFIG_PACKAGE_luci-app-quickstart=y"  >> ./.config
 
 
 
-#echo "CONFIG_PACKAGE_luci-theme-netgear=y"  >> ./.config  出错
 echo "CONFIG_PACKAGE_adguardhome=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-app-adguardhome=y"  >> ./.config
-# echo "CONFIG_PACKAGE_luci-app-advanced=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-app-ikoolproxy=y"  >> ./.config
 echo "CONFIG_PACKAGE_luci-app-netdata=y"  >> ./.config
 echo "CONFIG_PACKAGE_homebox=y"  >> ./.config
